a0499f32d45b2c9cdb037e690b487df4b22bd15f
loeumsalit-a11y <loeumsalit@gmail.com>
2026-05-06 21:36:27 +0700

    Update settings.py

--- posdb/posdb/settings.py
+++ posdb/posdb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&d=!zg98ru2l@#0r7d$h6bnzy4z5^t0b#69v!zncq1v!itofql
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['loemsalit123.pythonanywhere.com','localhost','127.0.0.1]
+ALLOWED_HOSTS = ['loemsalit123.pythonanywhere.com','localhost','127.0.0.1']
 
 
 # Application definition
